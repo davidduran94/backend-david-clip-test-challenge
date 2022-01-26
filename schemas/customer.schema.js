e76a4ae0b3617c61d1a89524552e515e91b0ab62
David Durán <dduran@scotiabank.com.mx>
2022-01-25 23:28:22 -0600

    feat: customer validation and service

---
+++ schemas/customer.schema.js
@@ -0,0 +1,36 @@
+const Joi = require("joi");
+
+const name = Joi.string().min(3).max(30);
+const email = Joi.string().email();
+const lastName = Joi.string();
+const phone = Joi.string();
+const address = Joi.object({
+  city: Joi.string(),
+  state: Joi.string(),
+  line1: Joi.string(),
+  line2: Joi.string(),
+  postal_code: Joi.string(),
+  country_code: Joi.string(),
+});
+
+const getCustomerSchema = Joi.object({});
+
+const createCustomerSchema = Joi.object({
+  name: name.required(),
+  lastName: lastName.required(),
+  phone: phone.required(),
+  email: email.required(),
+  address: address.required(),
+});
+
+const updateCustomerSchema = Joi.object({
+  name,
+  lastName,
+  phone,
+});
+
+module.exports = {
+  getCustomerSchema,
+  createCustomerSchema,
+  updateCustomerSchema,
+};
---
+++ services/customer.service.js
@@ -0,0 +1,17 @@
+const boom = require("@hapi/boom");
+
+class CustomerService {
+  constructor() {}
+
+  async find() {}
+
+  async findOne(id) {}
+
+  async create(data) {}
+
+  async update(id, changes) {}
+
+  async delete(id) {}
+}
+
+module.exports = CustomerService;
